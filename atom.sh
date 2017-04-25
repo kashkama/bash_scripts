126bead7bee491025f51800cb990e52a36538e41
kashkama <owen.kamau2014@gmail.com>
2017-04-25 15:32:00 +0300

    adding atom themes and packages installations

--- atom.sh
+++ atom.sh
@@ -8,4 +8,22 @@ sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
 sudo apt-get install atom
 
-echo "installing packages and themes"
+echo "installed atom"
+
+cd
+echo "installing themes and packages"
+
+apm install colorblind-wombat-dark-syntax
+apm install atom-beautify
+apm install atom-bootstrap3
+apm install atom-typescript
+apm install color-picker
+apm install file-icons
+apm install jshint
+apm install linter
+apm install linter-htmlhint
+apm install linter-jshint
+apm install minimap
+apm install pigments
+
+echo "installed all themes and packages"
