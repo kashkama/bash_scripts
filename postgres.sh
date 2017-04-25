5156083e3cb2cf2c3ff8122093582f09636e4d33
kashkama <owen.kamau2014@gmail.com>
2017-04-25 11:45:34 +0300

    adding the postgres lauch script with its ppa

--- postgres.sh
+++ postgres.sh
@@ -2,3 +2,11 @@
 
 # installing postgres
 echo "installing postgres"
+cd
+
+sudo apt-get install libpq-dev
+sudo add-apt-repository ppa:pitti/postgresql
+sudo apt-get update
+sudo apt-get install postgresql-9.2
+
+echo "installed postgres"
