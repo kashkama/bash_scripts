d6719e12f4b1b271e1aa014958e5e0bd04d1e85f
kashkama <owen.kamau2014@gmail.com>
2017-04-25 14:29:33 +0300

    adding youtube-dl lauch script for allowing downloading youtube videos

---
+++ youtube.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# installing youtube-dl allows youtube video download
+echo "installing youtube-dl"
+
+cd
+sudo apt-get install youtube-dl
+sudo apt-get update
+
+echo "installed youtube-dl"
