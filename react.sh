cac29cd9248ef4b3abd45f4b711b9ad2130581c2
kashkama <owen.kamau2014@gmail.com>
2017-06-15 11:02:02 +0300

    adding a react app launch script

---
+++ react.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "creating webpack powered react applications you need to install create-react-app library"
+echo "install node and npm first, check out how in the other bash scripts i have provived"
+cd
+npm install -g create-react-app
+
+echo "can create a react app by typint 'create-react-app  your-app' then npm start once you have 'cd' in your app"
--- yt.sh
+++ yt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # downloading videos from youtube simple and easy
 
+# make a directory yt in home directory using mkdir yt
 cd yt
 youtube-dl $1
 
