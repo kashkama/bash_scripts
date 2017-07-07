84244c052a5e2bdb588bdd0d9bdc0b1010528c56
kashkama <owen.kamau2014@gmail.com>
2017-07-07 09:07:53 +0300

    modifying all scripts

--- power.sh
+++ power.sh
@@ -10,6 +10,21 @@ echo " export PATH=~/bash:$PATH"
 cd
 sudo apt-get install ubuntu-restricted-extras
 sudo apt-get update
+
+#INSTRUCTIONS TO ACCESSING ALL SCRIPTS FROM ANY DIRCTORY.#######################################3
+mkdir ~/bin
+mv ~/*.sh ~/bin
+chmod +x ~/bin/*
+if ! grep 'PATH=.*HOME/bin' ~/.profile
+then
+    echo 'export PATH="$HOME/bin:$PATH"' >> ~/.profile
+fi
+. ~/.profile
+echo "$PATH"      # you should see your bin directory
+cd /tmp           # change to another random directory
+type myscript.sh  # see if the shell finds your scripts NOTE just try 
+
+#######################################################################
 # sudo apt-get  install vokoscreen
 # sudo apt-get pulseaudio
 
