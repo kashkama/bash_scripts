5f88cc87725c11285a839ad100c467b8e1c4564e
kashkama <owen.kamau2014@gmail.com>
2017-07-07 16:44:18 +0300

    how to install nodejs in ubuntu 16.04.2

--- npm.sh
+++ npm.sh
@@ -12,6 +12,16 @@ sudo apt-get install -y nodejs
 sudo apt intall npm
 sudo npm install -g bower
 sudo npm install -g ember-cli
+###############################################################
+# for linux 16.04.2
+sudo apt-get install curl build-essential gcc make
+curl -O https://nodejs.org/dist/v6.9.2/node-v6.9.2.tar.gz
+tar xzf node-v6.9.2.tar.gz
+cd node-v6.9.2
+./configure
+make
+sudo make install # installs this version of Node.js into a system directory
+####################################################################
 
 # install gulp globally
 sudo npm install gulp -g
