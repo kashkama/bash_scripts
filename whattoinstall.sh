1a9227728116f502114557b057d6051f4bef12f0
kashkama <owen.kamau2014@gmail.com>
2017-10-27 12:27:09 +0300

    adding applications to install on fresh distros

---
+++ whattoinstall.sh
@@ -0,0 +1,32 @@
+postman
+steam
+react
+gulp
+react
+code
+settings to code {
+
+    {
+    "files.autoSave": "afterDelay",
+    "workbench.iconTheme": "material-icon-theme",
+    "editor.fontFamily": "'Ubuntu Mono', 'Courier New', monospace, 'Droid Sans Fallback'",
+    "workbench.colorTheme": "Monokai",
+    "window.zoomLevel": 1,
+    "workbench.startupEditor": "newUntitledFile",
+    "editor.multiCursorModifier": "ctrlCmd"
+}
+
+installed themes :{
+    1227 theme
+    Beautify
+    Debugger for Chrome
+    Eslint
+    Html Boilerplate
+    Html snippets
+    JS standard linter
+    Material Icon theme
+    React-Beautify
+    vs color picker
+    
+}
+}
