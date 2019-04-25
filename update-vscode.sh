e6e8543332246937db898a9a41e270f745dbf6a2
kashkama <owen.kamau2014@gmail.com>
2019-04-25 11:37:36 +0300

    fixing the workbench visibility in the settings.json file

--- update-vscode.sh
+++ update-vscode.sh
@@ -12,16 +12,12 @@ echo "updated vscode"
     "workbench.iconTheme": "material-icon-theme",
     "workbench.colorTheme": "Monokai Dark Vibrant",
     "window.zoomLevel": -1.2,
-    "workbench.startupEditor": "newUntitledFile",
     "editor.multiCursorModifier": "ctrlCmd",
     "java.errors.incompleteClasspath.severity": "ignore",
-    "window.menuBarVisibility": "toggle",
     "breadcrumbs.enabled": false,
     "editor.detectIndentation": false,
     "javascript.updateImportsOnFileMove.enabled": "always",
     "liveServer.settings.donotShowInfoMsg": true,
-    "window.enableMenuBarMnemonics": false,
-    "window.titleBarStyle": "native",
     "editor.suggestSelection": "first",
     "vsintellicode.modify.editor.suggestSelection": "automaticallyOverrodeDefaultValue",
     "files.exclude": {
@@ -31,52 +27,51 @@ echo "updated vscode"
         "**/.factorypath": true
     },
     "terminal.integrated.fontWeightBold": "normal",
-    /*                 xcode settings                     */ 
+  /*                 xcode settings                     */ 
     "editor.cursorStyle": "line-thin",
-  "editor.fontFamily": "'Ubuntu Mono'",
-  "editor.fontLigatures": true,
-  "editor.fontSize": 18,
-  "editor.fontWeight": "400",
-  "editor.lineHeight": 20,
-  "terminal.integrated.fontSize": 15.5,
-  "terminal.integrated.lineHeight": 1.23,
-  "editor.minimap.enabled": true,
-  "editor.minimap.renderCharacters": false,
-  "editor.overviewRulerBorder": false,
-  "editor.renderIndentGuides": false,
-  "editor.renderLineHighlight": "all",
-  "workbench.activityBar.visible": false,
-  "workbench.editor.tabCloseButton": "right",
-  "workbench.editor.showIcons": false,
-  "window.nativeTabs": true,
-  "editor.tokenColorCustomizations": {
-    "[Xcode Partial (Light)]": {
-      "textMateRules": [
-        {
-          "scope": "comment",
-          "settings": {
-            "foreground": "#536579",
-            "fontStyle": "italic"
-          }
-        }
-      ]
-    },
-    "[Monokai Dark Vibrant]": {
-      "textMateRules": [
-        {
-          "scope": "source",
-          "settings": {
-            "foreground": "#FFFFFF"
-          }
+    "editor.fontFamily": "'Ubuntu Mono'",
+    "editor.fontLigatures": true,
+    "editor.fontSize": 18,
+    "editor.fontWeight": "400",
+    "editor.lineHeight": 20,
+    "terminal.integrated.fontSize": 15.5,
+    "terminal.integrated.lineHeight": 1.23,
+    "editor.minimap.enabled": true,
+    "editor.minimap.renderCharacters": false,
+    "editor.overviewRulerBorder": false,
+    "editor.renderIndentGuides": false,
+    "editor.renderLineHighlight": "all",
+    "workbench.activityBar.visible": true,
+    "workbench.editor.tabCloseButton": "right",
+    "workbench.editor.showIcons": false,
+    "editor.tokenColorCustomizations": {
+        "[Xcode Partial (Light)]": {
+        "textMateRules": [
+            {
+            "scope": "comment",
+            "settings": {
+                "foreground": "#536579",
+                "fontStyle": "italic"
+            }
+            }
+        ]
         },
-        {
-          "scope": "comment",
-          "settings": {
-            "foreground": "#6C7986",
-            "fontStyle": "italic"
-          }
+        "[Monokai Dark Vibrant]": {
+        "textMateRules": [
+            {
+            "scope": "source",
+            "settings": {
+                "foreground": "#FFFFFF"
+            }
+            },
+            {
+            "scope": "comment",
+            "settings": {
+                "foreground": "#6C7986",
+                "fontStyle": "italic"
+            }
+            }
+        ]
         }
-      ]
     }
-  }
 }
